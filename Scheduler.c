9ae9dcb352a897e707a1c7934b327c90ac188f25
Shubham2022491 <119068073+Shubham2022491@users.noreply.github.com>
2023-10-16 23:36:53 +0530

    Update Scheduler.c

--- Scheduler.c
+++ Scheduler.c
@@ -1,201 +1,233 @@
-// Modify the SimpleShell code
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
 #include <signal.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <semaphore.h>
 
-int NCPU , TSLICE;
+#define MAX_QUEUE_SIZE 100
+#define MAX_EXECUTABLE_LENGTH 256
+
+sem_t readyqueue_mutex;
+sem_t runningqueue_mutex;
+sem_t terminatedqueue_mutex;
+
+int NCPU, TSLICE;
 
 typedef struct {
     int pid;
-    int burst_time; // Time needed to complete the process
-    int remaining_time; // Remaining time to complete the process
-    int state; // Process state (e.g., READY, RUNNING, TERMINATED)
-    char *executable;
+    int remaining_time;
+    int state;
+    char executable[MAX_EXECUTABLE_LENGTH];
 } Process;
 
-// Attach to the shared memory segment (shm_id is the shared memory ID)
-void* shared_memory = shmat(shm_id, NULL, 0);
+typedef struct {
+    Process queue[MAX_QUEUE_SIZE];
+    int front;
+    int rear;
+    int itemCount;
+} SharedQueue;
 
-struct {
-    pid_t pid;
-    char executable[256]; // Adjust the buffer size as needed
-    int NCPU;
-    int TSLICE;
-    int scheduler_pid; 
-    //int signal_start; // Signal to start a process
-    //int signal_stop;  // Signal to stop a process                                                                                 //  dbt what will be the data type 
-} *shared_data = (struct shared_data*)shared_memory;
+SharedQueue *readyqueue;
+SharedQueue *runningqueue;
+SharedQueue *terminatedqueue;
 
-int status_pipe[2]; // Pipe for receiving process status from the shell
-
-// Define a circular queue to store processes
-Process ready_queue[MAX_QUEUE_SIZE];
-int front = 0;
-int rear = -1;
-int itemCount = 0;
-
-// Function to add a process to the ready queue
-void enqueue(Process process) {
-    if (itemCount < MAX_QUEUE_SIZE) {
-        if (rear == MAX_QUEUE_SIZE - 1)
-            rear = -1;
-        ready_queue[++rear] = process;
-        itemCount++;
+void start_execution(int signo) {
+    if (signo == SIGUSR2) {
+        int front = readyqueue->front;
+        char *s = readyqueue->queue[front].executable;
+        execlp("sh", "sh", "-c", s, NULL);
     }
 }
 
-// Function to remove and return a process from the front of the queue
-Process dequeue() {
-    Process data = ready_queue[front++];
-    if (front == MAX_QUEUE_SIZE)
-        front = 0;
-    itemCount--;
-    return data;
-}
+int fd_ready, fd_running, fd_terminated;
 
+void terminate_shell(int signo) {
+    printf("Processes in the terminated queue:\n");
+    if (signo == SIGINT) {
+        while (terminatedqueue->itemCount > 0) {
+            Process terminatedProcess = terminatedqueue->queue[++terminatedqueue->front];
+            printf("PID: %d, Executable: %s\n", terminatedProcess.pid, terminatedProcess.executable);
+            terminatedqueue->itemCount--;
+        }
 
-// Define a circular queue to store running processes
-Process running_queue[MAX_QUEUE_SIZE];
-int running_front = 0;
-int running_rear = -1;
-int running_itemCount = 0;
+        munmap(readyqueue, sizeof(SharedQueue));
+        munmap(runningqueue, sizeof(SharedQueue));
+        munmap(terminatedqueue, sizeof(SharedQueue));
 
-// Function to add a process to the running queue
-void enqueueRunning(Process process) {
-    if (running_itemCount < MAX_QUEUE_SIZE) {
-        if (running_rear == MAX_QUEUE_SIZE - 1)
-            running_rear = -1;
-        running_queue[++running_rear] = process;
-        running_itemCount++;
+        close(fd_ready);
+        close(fd_running);
+        close(fd_terminated);
+
+        shm_unlink("/readyqueue");
+        shm_unlink("/runningqueue");
+        shm_unlink("/terminatedqueue");
+
+        exit(EXIT_SUCCESS);
+        exit(0);
     }
 }
 
-// Function to remove and return a process from the front of the running queue
-Process dequeueRunning() {
-    Process data = running_queue[running_front++];
-    if (running_front == MAX_QUEUE_SIZE)
-        running_front = 0;
-    running_itemCount--;
-    return data;
-}
+int main(int argc, char *argv[]) {
+    NCPU = atoi(argv[1]);
+    TSLICE = atoi(argv[2]);
+    int pid;
+    printf("Command line inputs stored: %d %d\n", NCPU, TSLICE);
+    int custom_signal = SIGUSR2;
 
-// Define a circular queue to store terminated processes
-Process terminated_queue[MAX_QUEUE_SIZE];
-int terminated_front = 0;
-int terminated_rear = -1;
-int terminated_itemCount = 0;
-
-// Function to add a process to the terminated queue
-void enqueueTerminated(Process process) {
-    if (terminated_itemCount < MAX_QUEUE_SIZE) {
-        if (terminated_rear == MAX_QUEUE_SIZE - 1)
-            terminated_rear = -1;
-        terminated_queue[++terminated_rear] = process;
-        terminated_itemCount++;
+    int fd_ready = shm_open("/readyqueue", O_CREAT | O_RDWR, 0666);
+    if (fd_ready == -1) {
+        perror("shm_open readyqueue");
+        exit(EXIT_FAILURE);
     }
-}
 
-// Function to remove and return a process from the front of the terminated queue
-Process dequeueTerminated() {
-    Process data = terminated_queue[terminated_front++];
-    if (terminated_front == MAX_QUEUE_SIZE)
-        terminated_front = 0;
-    terminated_itemCount--;
-    return data;
-}
+    if (ftruncate(fd_ready, sizeof(SharedQueue)) == -1) {
+        perror("ftruncate readyqueue");
+        exit(EXIT_FAILURE);
+    }
 
-void signal_start_execution(int signo) {
-    if (signo == SIGSUR2) {
-        while(1){
-            // Start executing the processes
-            if (itemCount != 0){
-                int num_to_run = (itemCount < NCPU) ? itemCount : NCPU;
+    readyqueue = (SharedQueue *)mmap(NULL, sizeof(SharedQueue), PROT_READ | PROT_WRITE, MAP_SHARED, fd_ready, 0);
+    if (readyqueue == MAP_FAILED) {
+        perror("mmap readyqueue");
+        exit(EXIT_FAILURE);
+    }
+
+    readyqueue->front = -1;
+    readyqueue->rear = -1;
+    readyqueue->itemCount = 0;
+
+    int fd_running = shm_open("/runningqueue", O_CREAT | O_RDWR, 0666);
+    if (fd_running == -1) {
+        perror("shm_open runningqueue");
+        exit(EXIT_FAILURE);
+    }
+
+    if (ftruncate(fd_running, sizeof(SharedQueue)) == -1) {
+        perror("ftruncate runningqueue");
+        exit(EXIT_FAILURE);
+    }
+
+    runningqueue = (SharedQueue *)mmap(NULL, sizeof(SharedQueue), PROT_READ | PROT_WRITE, MAP_SHARED, fd_running, 0);
+    if (runningqueue == MAP_FAILED) {
+        perror("mmap runningqueue");
+        exit(EXIT_FAILURE);
+    }
+
+    runningqueue->front = -1;
+    runningqueue->rear = -1;
+    runningqueue->itemCount = 0;
+
+    int fd_terminated = shm_open("/terminatedqueue", O_CREAT | O_RDWR, 0666);
+    if (fd_terminated == -1) {
+        perror("shm_open terminatedqueue");
+        exit(EXIT_FAILURE);
+    }
+
+    if (ftruncate(fd_terminated, sizeof(SharedQueue)) == -1) {
+        perror("ftruncate terminatedqueue");
+        exit(EXIT_FAILURE);
+    }
+
+    terminatedqueue = (SharedQueue *)mmap(NULL, sizeof(SharedQueue), PROT_READ | PROT_WRITE, MAP_SHARED, fd_terminated, 0);
+    if (terminatedqueue == MAP_FAILED) {
+        perror("mmap terminatedqueue");
+        exit(EXIT_FAILURE);
+    }
+
+    terminatedqueue->front = -1;
+    terminatedqueue->rear = -1;
+    terminatedqueue->itemCount = 0;
+
+    sem_init(&readyqueue_mutex, 1, 1);
+    sem_init(&runningqueue_mutex, 1, 1);
+    sem_init(&terminatedqueue_mutex, 1, 1);
+
+    signal(SIGINT, terminate_shell);
+
+    pid = fork();
+    if (pid < 0) {
+        perror("fork failed!");
+    } else if (pid == 0) {
+        printf("Scheduler started\n");
+
+        while (1) {
+            if (readyqueue->itemCount != 0) {
+                printf("Item count as seen by scheduler: %d\n",readyqueue->itemCount);
+                int num_to_run = (readyqueue->itemCount < NCPU) ? readyqueue->itemCount : NCPU;
+                printf("Scheduler sends signal to start\n");
                 for (int i = 0; i < num_to_run; i++) {
-                    Process process = dequeue();
-                    process.state = 1; // Set the state to RUNNING
-                    // Notify SimpleShell to start the process
-                    // Add the running process to the running queue
-                    enqueueRunning(process);
+                    printf("Check1\n");
+                    Process process = readyqueue->queue[++readyqueue->front];
+                    printf("Check2\n");
+                    readyqueue->itemCount--;
+                    printf("Check3\n");
+                    process.state = 1;
+                    printf("Check4\n");
+                    runningqueue->queue[++runningqueue->rear] = process;
+                    printf("Check5\n");
+                    runningqueue->itemCount++;
                     kill(process.pid, SIGUSR2);
+                    printf("SCHEDULER signal sent to start\n");
                 }
-                // Simulate the time slice (TSLICE) by sleeping
-                sleep(TSLICE);
-                // Notify SimpleShell to stop the processes
+                printf("Scheduler sleeps\n");
+                sleep(TSLICE / 1000);
+                printf("Scheduler sends signal to stop\n");
                 for (int i = 0; i < num_to_run; i++) {
-                    Process process = dequeueRunning();
-                    process.state = 0; // Set the state to READY
-                    // Notify SimpleShell to stop the process
-                    int status;
-                    read(status_pipe[0], &status, sizeof(status));
-                    if (status != 0){
-                        kill(process.pid, SIGUSR2);
-                        enqueue(process);
+                    Process process = runningqueue->queue[++runningqueue->front];
+                    runningqueue->itemCount--;
+                    process.state = 0;
+                    int status = 0;
+                    printf("hi\n");
+                    if (waitpid(process.pid, &status, WNOHANG) == 0) {
+                        kill(process.pid, SIGSTOP);
+                        sem_wait(&readyqueue_mutex);
+                        readyqueue->queue[++readyqueue->rear] = process;
+                        readyqueue->itemCount++;
+                        sem_post(&readyqueue_mutex);
+                        printf("Hey I'm going in readyqueue process: %s", process.executable);
+                    } else {
+                        terminatedqueue->queue[++terminatedqueue->rear] = process;
+                        terminatedqueue->itemCount++;
+                        printf("Hey I'm terminated process: %s\n", process.executable);
                     }
-                    else if (status == 0){
-                        enqueueTerminated(process);
-                    }
+                    printf("status in scheduler: %d\n", status);
+                    printf("SCHEDULER signal sent to stop\n");
                 }
             }
-        }    
-    }
-}
-
-// Signal handler to catch the signal sent by the SimpleShell
-void signal_handler(int signo) {
-    if (signo == SIGUSR1) {
-        // Example:
-        Process newProcess;
-        // Extract the PID and executable
-        NCPU = shared_data->NCPU;
-        TSLICE = shared_data->TSLICE;
-        newProcess.pid = shared_data->pid;
-        strcpy(newProcess.executable, shared_data->executable);
-        newProcess.state = 0; // Set the state to READY
-
-        // Add the process to the ready queue
-        enqueue(newProcess);
-    }
-}
-
-int main(int argc, char* argv[]) {
-    // ...
-
-
-    // Set up signal handlers to catch signals from SimpleShell
-    signal(SIGUSR1, signal_handler);
-    signal(SIGUSR2, signal_start_execution);
-
-    // Start the SimpleScheduler daemon as a separate process
-    pid_t scheduler_pid = fork();
-
-    if (scheduler_pid < 0) {
-        perror("fork");
-        exit(EXIT_FAILURE);
-    } else if (scheduler_pid == 0) {
-        // This code runs in the SimpleScheduler daemon
-//
-    //    // Execute the SimpleScheduler code here
-        execl("./simplescheduler", "simplescheduler", NULL); // Use the correct path to the SimpleScheduler executable
-        perror("execl");
-        exit(EXIT_FAILURE);
+        }
     } else {
-        shared_data->scheduler_pid = scheduler_pid;
-    //    // This code runs in the SimpleShell
-//          
-    //    // Store the process ID of the SimpleScheduler daemon
-    //    // You can use this ID to send signals to the SimpleScheduler
-    //    // ...
-//
-    //    // Continue with the SimpleShell logic
+        while (1) {
+            char command[100];
+            printf("SimpleShell$ ");
+            fgets(command, sizeof(command), stdin);
+            command[strcspn(command, "\n")] = '\0';
+
+            if (strncmp(command, "submit ", 7) == 0) {
+                char *executable = command + 7;
+                int childs_pid = fork();
+                if (childs_pid < 0) {
+                    perror("fork");
+                } else if (childs_pid == 0) {
+                    signal(SIGUSR2, start_execution);
+                    pause();
+                } else {
+                    Process newProcess;
+                    newProcess.pid = childs_pid;
+                    strncpy(newProcess.executable, executable, MAX_EXECUTABLE_LENGTH);
+                    newProcess.state = 0;
+                    sem_wait(&readyqueue_mutex);
+                    readyqueue->queue[++readyqueue->rear] = newProcess;
+                    readyqueue->itemCount++;
+                    sem_post(&readyqueue_mutex);
+                }
+            }
+        }
     }
-
-    // ...
-    // Detach from shared memory
-    shmdt(shared_memory);
-
     return 0;
 }
